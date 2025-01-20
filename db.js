c1001f822d1ebd1ee05c001b73522e6a7db71ed3
kapilver <128472629+kapilver@users.noreply.github.com>
2025-01-20 09:11:31 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,43 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// // Define the MongoDB connection URL
+// const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+// // const mongoURL = process.env.MONGODB_URL;
+
+// // Set up MongoDB connection
+// mongoose.connect(mongoURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+// // Get the default connection
+// // Mongoose maintains a default connection object representing the MongoDB connection.
+// const db = mongoose.connection;
+
+// // Define event listeners for database connection
+
+// db.on('connected', () => {
+//     console.log('Connected to MongoDB server');
+// });
+
+// db.on('error', (err) => {
+//     console.error('MongoDB connection error:', err);
+// });
+
+// db.on('disconnected', () => {
+//     console.log('MongoDB disconnected');
+// });
+
+// // Export the database connection
+// module.exports = db;
+
+
+
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://kapilvermajaika:verma131996@cluster0.led03qz.mongodb.net/voting").then(() => console.log(" database connected")).catch((err) => console.log("Error ---------- " + err));
+
+
+
+
